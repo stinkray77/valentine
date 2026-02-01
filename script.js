3badb3a9b071b272acd8c45d0cafd7ee8eedb9a9
ray <raychua0106@gmail.com>
2026-02-02 03:41:22 +0800

    Edit buttons

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ const question = document.querySelector(".question")
 const gif = document.querySelector(".gif")
 const yesBtn = document.querySelector(".yes-btn")
 const noBtn = document.querySelector(".no-btn")
+let clickCount = 0; // track how many times yes is clicked
+let scale = 1; // inital scale of yes button
 
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Yayyy";
-    gif.src="https://gifdb.com/images/high/pocoyo-nervous-pink-elly-ffg9br17wldgl1mc.gif"
-})
+    clickCount++;
+
+    if (clickCount >= 5) {
+        // Change the content or redirect after 5 clicks
+        question.innerHTML = "Yayyy! Finally!";
+        gif.src = "https://cdn.dribbble.com/userupload/32237563/file/original-f7f23a20ba688a63416b509924138ee9.gif"
+    } else {
+        scale += 0.5;
+        yesBtn.style.transform = `scale(${scale})`;
+    }
+});
 
 noBtn.addEventListener("mouseover", () => {
     const noBtnRect = noBtn.getBoundingClientRect();
@@ -16,6 +26,7 @@ noBtn.addEventListener("mouseover", () => {
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
+    noBtn.style.position = "absolute";
     noBtn.style.left = randomX + "px";
     noBtn.style.top = randomY + "px";
 })
