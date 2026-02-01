70fa69a739338e158833d7bc9a73c65e0e0d569c
ray <raychua0106@gmail.com>
2026-02-02 04:29:42 +0800

    Try to fix buttons

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ noBtn.addEventListener("mouseover", () => {
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
-    noBtn.style.position = "absolute";
     noBtn.style.left = randomX + "px";
     noBtn.style.top = randomY + "px";
 })
